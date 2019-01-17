fb3a52185404ab8b7f2ab0cb5e44f5157b8e36cc
wayaca <wayaca@ifsworld.com>
2019-01-17 13:59:43 -0500

    add a list

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-    This example shows how to use React Router!
-  </div>
-);
+const numbers = [1, 2, 23, 3, 4, 5];
+// const About = () => (
+//   <div>
+//     <h2>About</h2>
+//     This example shows how to use React Router!
+//   </div>
+// );
+
+function About() {
+  const listItems = numbers.map((number) => <li>{number}</li>);
+  return (<ul>{listItems}</ul>);
+}
 
 export default About;
